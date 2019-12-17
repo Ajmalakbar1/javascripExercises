8b92071a7d4879ec48f02ba71f1032c86fe3291f
Ajmal <ajmaljannn@gmail.com>
2019-12-17 15:06:53 -0500

    it was a bit hard

---
+++ 02-maths/03-even-square/script.js
@@ -0,0 +1,26 @@
+/* becode/javascript
+ *
+ * /02-maths/03-even-square/script.js - 2.3: carrés des pairs
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(function() {
+
+    document.getElementById("run").addEventListener("click", function() {
+        // your code here
+
+        let j = "";
+        let i;
+
+        for (let i = 1; i <= 21; i++) {
+            j += Math.sqrt(i) +"  ";
+        }
+        alert(j);
+    });
+
+})();
