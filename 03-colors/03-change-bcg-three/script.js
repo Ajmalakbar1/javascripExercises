56709cbe3e0140aba983adf6253147c7dbab71fe
Ajmal <ajmaljannn@gmail.com>
2019-12-18 08:33:58 -0500

    cool random math function

--- 03-colors/03-change-bcg-three/script.js
+++ 03-colors/03-change-bcg-three/script.js
@@ -13,4 +13,8 @@
 
     // your code here
 
+    document.getElementById("run").addEventListener("click", function() {
+        let randomBackgroundColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+        document.body.style.backgroundColor = randomBackgroundColor;
+    });
 })();
