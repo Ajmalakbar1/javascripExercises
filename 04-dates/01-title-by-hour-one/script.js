8c2989f2c51e249d652838fec25e2a8cb721a2de
Ajmal <ajmaljannn@gmail.com>
2019-12-19 05:08:44 -0500

    hello world

--- 04-dates/01-title-by-hour-one/script.js
+++ 04-dates/01-title-by-hour-one/script.js
@@ -9,10 +9,23 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+
 
     // to change the content of a tag: document.getElementById("element-id").innerHTML = "new-value"
 
     // your code here
+
+(function() {
+
+    var time = new Date().getHours();
+
+    if (time < 18) {
+        greeting = "Hello, world"}
+
+    else if (time > 18) {
+        greeting = "Good evening"}
+
+    document.getElementById("target").innerHTML = greeting;
+
 
 })();
