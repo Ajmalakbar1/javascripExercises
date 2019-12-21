4edc1b6ef1de1e6b29e8f5ae6ff089e3afc2f7ae
Ajmal <ajmaljannn@gmail.com>
2019-12-21 23:21:55 +0100

    finally i did it

--- 04-dates/03-age-by-select/script.js
+++ 04-dates/03-age-by-select/script.js
@@ -9,17 +9,28 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+//(function (){
 
     // your code here
     // trying to find some
-    (function() {
-        varp person = {
-            name:"stephan",
-            lastname:"van del",
-            bDay: document.getElementById("dob-day").value,
-            bMonth: document.getElementById("dob-month").value,
-            bYear: document.getElementById("dob-year").value,
-        };
-        
-    })();
+
+       // let diff_ms = Date.now() - dob.getTime();
+       // let age_dt = new Date(diff_ms);
+
+        //return Math.abs(age_dt.getUTCFullYear() - 1970);
+
+    //alert(calcAge(new Date(1982, 11, 4)));
+
+   // alert(calcAge(new Date(1962, 1, 1)));
+
+
+
+//});
+function calcAge ()
+{
+    let dobDate = new Date( document.getElementById("dob-year").value,document.getElementById("dob-month").value -1,document.getElementById("dob-day").value);
+    let Datediff = Date.now() - dobDate.getTime();
+    let agedt = new Date(Datediff);
+    let agee = Math.abs(agedt.getUTCFullYear() - 1970);
+    alert("Congratulation you are "+agee +" years old.");
+}
