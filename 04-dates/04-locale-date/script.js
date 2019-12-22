ba99476ea48e96d08a8a278d931dbff73bd6dad2
Ajmal <ajmaljannn@gmail.com>
2019-12-22 01:11:10 +0100

    was to hard

--- 04-dates/04-locale-date/script.js
+++ 04-dates/04-locale-date/script.js
@@ -15,18 +15,59 @@
 
     // your code here
 
-(function() {
 
-    // https://stackoverflow.com/questions/3552461/how-to-format-a-javascript-date
-    //https://reactgo.com/format-date-time-javascript/
-    var options = { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric' };
-    var options2 = { hour12 : false, hour:  "numeric", minute: "numeric"};
-    var today  = new Date();
-    date = today.toLocaleDateString("en-GB", options);
-    time = today.toLocaleTimeString("en-GB", options2);
-    date = date.replace(',','');
-    time = time.replace(':','h');
-    out = date+", "+time;
-    document.getElementById("target").innerHTML = out;
+//<p>Change the content of the indicated tag by the date of today, in English (ex : Sunday 28 October 2018, 23h32).</p>
+function findDate() {
 
-})();
+    //let gen = document.getElementById("target").innerHTML = new Date().toLocaleString();
+    let getDay = new Date().getDay();
+    let myDay;
+   if (getDay == "0")
+   {
+       myDay ="Saturday";
+   }else if(getDay == "1"){
+       myDay ="Sunday";
+   }else if(getDay=="2"){
+       myDay="Monday";
+    }else if(getDay=="3"){
+       myDay="Tuesday";
+    }else if(getDay=="4"){
+       myDay="Wednesday";
+   }else if(getDay=="5"){
+       myDay="Thursday";
+   }else if(getDay=="6"){
+       myDay="Friday";
+   }
+   let myMonth;
+   let getMonth = new Date().getMonth();
+
+    if (getMonth=="0"){
+        myMonth="January";
+    }else if (getMonth=="1"){
+        myMonth ="February";
+    }else if(getMonth=="2"){
+        myMonth="March";
+    }else if (getMonth=="3"){
+        myMonth="April"
+    }else if (getMonth=="4"){
+        myMonth="May";
+    }else if (getMonth=="5"){
+        myMonth="June";
+    }else if (getMonth=="6"){
+        myMonth="July";
+    }else if (getMonth=="7"){
+        myMonth="August";
+    }else if (getMonth=="8"){
+        myMonth="September";
+    }else if (getMonth=="9"){
+        myMonth="October";
+    }else if (getMonth=="10"){
+        myMonth="November";
+    }else if (getMonth=="11"){
+        myMonth="December";
+    }
+
+
+
+    document.getElementById("target").innerHTML = myDay+ " "+new Date().getDate()+" "+ myMonth+" "+  new Date().getFullYear()+", "+ new Date().getHours()+"h"+ new Date().getUTCMinutes();
+}
