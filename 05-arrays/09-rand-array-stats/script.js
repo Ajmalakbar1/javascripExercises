bc7de3fb528ef4454b95cba06128f4c3b15172d5
Ajmal <ajmaljannn@gmail.com>
2019-12-23 16:31:50 +0100

    5.9

--- 05-arrays/09-rand-array-stats/script.js
+++ 05-arrays/09-rand-array-stats/script.js
@@ -11,6 +11,48 @@
 
 (function() {
 
-    // your code here
+    document.getElementById("run").addEventListener("click", function () {
+        let numbers = [];
+
+        for ( let i=1; i<11; i++ ){
+            let randomNumber = Math.floor(Math.random() * 100);
+            //console.log(randomNumber);
+
+            numbers.push(randomNumber);
+            //console.log(numbers);
+
+            document.getElementById("n-"+i).innerHTML = numbers[i-1];
+            //console.log("n-"+i)
+
+        }
+
+        console.log(numbers);
+
+        const arrMin = Math.min(...numbers);
+        console.log(arrMin);
+
+        document.getElementById("min").innerHTML = arrMin;
+        console.log("min");
+
+        const arrMax = Math.max(...numbers);
+        console.log(arrMax);
+
+        document.getElementById("max").innerHTML = arrMax;
+        console.log("max");
+
+        const arrSum = numbers.reduce((totalValue,inputValue) => totalValue + inputValue, 0);
+        console.log(arrSum);
+        document.getElementById("sum").innerHTML = arrSum;
+
+        const arrAvg = arrSum / numbers.length;
+        document.getElementById("average").innerHTML = arrAvg;
+
+    })
 
 })();
+
+
+
+        // let uno = document.getElementById("n-1").innerHTML = Math.floor(Math.random()*100)+1;
+
+
