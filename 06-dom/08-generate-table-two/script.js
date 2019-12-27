79d15b5c4a08714b9206d4964aada06e226d5119
Ajmal <ajmaljannn@gmail.com>
2019-12-27 15:20:30 +0100

    6.9

--- 06-dom/08-generate-table-two/script.js
+++ 06-dom/08-generate-table-two/script.js
@@ -12,5 +12,17 @@
 (function() {
 
     // your code here
-
+document.getElementById("target").addEventListener("load",myFunction());
+    function myFunction(){
+    var i = 0;
+    document.write('<table border="1" cellspacing="0" bgcolor="#ff7f50" id="Tabula">');
+    for (i = 1; i < 11; i++) {
+        document.write("<tr>");
+        for (j = 1; j < 11; j++) {
+            document.write("<td>" + i + " x " + j + " = " + j * i + "</td>");
+        }
+        document.write("</tr>");
+    }
+    document.write("</table>");
+}
 })();
