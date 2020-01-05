8b784d9a73801af4ea497d943aa76fbe8a0fe902
Ajmal <ajmaljannn@gmail.com>
2020-01-05 14:44:55 +0100

    was to hard and i make that by help of a friend

--- 07-misc/01-waving-text/script.js
+++ 07-misc/01-waving-text/script.js
@@ -9,8 +9,18 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
-
-    // your code here
-
-})();
+//(function() {
+//document.getElementById("target").addEventListener("load",function () {
+function myFunc(){
+    let Len = document.getElementById("target").innerText.length;
+    let txt = document.getElementById("target").innerText;
+    let res ="";
+    let i;
+    for(i=0;i <= Len;){
+        let b = txt.charAt(i);
+        res +=b.fontsize(1+i);
+        i++;
+    }
+    document.getElementById("target").innerHTML=res;
+}
+//})();
