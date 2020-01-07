549b6924bfc6c8fefae8e265672b66389d6da54c
Ajmal <ajmaljannn@gmail.com>
2020-01-07 10:18:50 +0100

    7.5

--- 07-misc/05-worst-ui-three/script.js
+++ 07-misc/05-worst-ui-three/script.js
@@ -12,5 +12,59 @@
 (function() {
 
     // your code here
+    let a="";
+
+    document.getElementById("fix-part-one").addEventListener("click",function () {
+
+        do {
+            a = Math.floor(Math.random() * 480) + 2;
+        }while(a<=400)
+        document.getElementById("target").innerHTML = "0"+a;
+        document.getElementById("part-one").value = "0"+a;
+        document.getElementById("part-two").value = "00";
+        document.getElementById("part-three").value = "00";
+        document.getElementById("part-four").value = "00";
+    })
+
+    document.getElementById("fix-part-two").addEventListener("click",myFunc);
+    document.getElementById("fix-part-three").addEventListener("click",myFunc2);
+    document.getElementById("fix-part-four").addEventListener("click", myFunc3);
+    function myFunc(){
+        let c =document.getElementById("target").innerText;
+        if(c.length<10) {
+            let b = "";
+            do {
+                b = Math.floor(Math.random() * 100) + 2;
+            } while (b < 10)
+
+            document.getElementById("target").innerHTML = c + b;
+            document.getElementById("part-two").value = b;
+        }
+    }
+    function myFunc2(){
+        let c =document.getElementById("target").innerText;
+        if(c.length<10) {
+            let b = "";
+            do {
+                b = Math.floor(Math.random() * 100) + 2;
+            } while (b < 10)
+
+            document.getElementById("target").innerHTML = c + b;
+            document.getElementById("part-three").value = b;
+        }
+    }
+    function myFunc3(){
+        let c =document.getElementById("target").innerText;
+        if(c.length<10) {
+            let b = "";
+            do {
+                b = Math.floor(Math.random() * 100) + 2;
+            } while (b < 10)
+
+            document.getElementById("target").innerHTML = c + b;
+            document.getElementById("part-four").value = b;
+        }
+    }
+
 
 })();
