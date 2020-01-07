f0663361a6b6ce94a011fec66174b849c85f9baa
Ajmal <ajmaljannn@gmail.com>
2020-01-07 10:43:18 +0100

    7.6

--- 07-misc/06-guess-number/script.js
+++ 07-misc/06-guess-number/script.js
@@ -12,5 +12,23 @@
 (function() {
 
     // your code here
-
+    document.getElementsByTagName("body")[0].onload=function () {
+        myFunc()
+};
+    function myFunc() {
+        let inpt;
+        let msg ="Guess a number between 1 to 100 ";
+        let ran;
+        do {
+            inpt = prompt(msg,);
+             ran = Math.floor(Math.random() * 100) + 2;
+             msg="";
+             if(Number(inpt) > ran){
+                 msg = "Oops...Higher! Guess another Number";
+             }else{
+                 msg = "Oops...lower! Guess another Number";
+             }
+        }while(Number(inpt)!=ran)
+        alert("Congratulation! You guessed the right number. " + ran);
+    }
 })();
