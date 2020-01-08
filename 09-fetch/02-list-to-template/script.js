667bf1046803319813e7ac85dc9c905b6cfdcd6d
Ajmal <ajmaljannn@gmail.com>
2020-01-08 10:16:54 +0100

    by helpin of steve

--- 09-fetch/02-list-to-template/script.js
+++ 09-fetch/02-list-to-template/script.js
@@ -11,4 +11,21 @@
 
 (() => {
     // your code here
+    document.getElementById("run").addEventListener("click", function(){
+
+        fetch("http://localhost:12345/_shared/api.json")
+            .then(response => response.json())
+            .then(data => {
+               let tem = document.getElementById("tpl-hero");
+               for (let i = 0; i <data.heroes.length ; i++){
+                   let copy =tem.content.cloneNode(true);
+                   copy.querySelector(".name").innerHTML = data.heroes[i].name;
+                   copy.querySelector(".alter-ego").innerHTML = data.heroes[i].alterEgo;
+                   copy.querySelector(".powers").innerHTML = data.heroes[i].abilities;
+                   document.getElementById("target").appendChild(copy);
+
+
+               }
+            })
+    });
 })();
